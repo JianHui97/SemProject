eb4ef4a2712a5a9d8ba8c9b96cce6f250f743d36
jiasoonn <66350135+jiasoonn@users.noreply.github.com>
2020-07-26 23:44:38 +0800

    Update header1.php

--- sdw/ManageBooth/View/header1.php
+++ sdw/ManageBooth/View/header1.php
@@ -31,6 +31,7 @@
              <li><a href="/sdw/ManageEvaluation/view/InsertJuryForm.php">Jury</a></li>
              <li><a href="/sdw/ManageEquipment/manage_equipment/viewequip.php">Equipment</a></li>
              <li><a href="/sdw/ManageBooth/View/BoothView.php">Booth</a></li>
+			 <li><a href="">Annoucement</a></li>
 			 <li><a href="http://localhost/sdw/ManageLogin/View/">Logout</a></li>
            </ul>
           </nav>

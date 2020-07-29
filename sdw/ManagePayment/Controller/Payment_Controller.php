99d61986a2488b538859be3bb4c9f5aac0484998
NORHABSAH ALI <nor.habsah97@gmail.com>
2020-07-29 12:24:05 +0800

    fixing the error of code

--- sdw/ManagePayment/Controller/Payment_Controller.php
+++ sdw/ManagePayment/Controller/Payment_Controller.php
@@ -19,17 +19,4 @@ class paymentreceipt
 <?php
 require_once '../Model/Payment_Detail_Model.php';
 
-class paymentdetail
-{
-
-	public function viewdetail()
-	{
-
-		$details = new modeldetail();
-
-		$resultset = $details->pp();
-		return $resultset;
-	}
-
-}
 ?>

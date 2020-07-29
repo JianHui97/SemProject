302e6430c1ea6145779bef5fb791e4c71f08d80b
shafirul <shafirulnazmy@gmail.com>
2020-07-29 14:55:16 +0800

    fix bug

--- sdw/ManagePayment/Model/Payment_Detail_Model.php
+++ sdw/ManagePayment/Model/Payment_Detail_Model.php
@@ -19,7 +19,7 @@ class modeldetail
     	die("Connection failed: " . $conn->connect_error);
 	} 
 
-			$sql = "SELECT * FROM equipment INNER JOIN participants ON equipment.ParticipantID = participants.ParticipantID WHERE EquipmentStatus = 'Reject'";
+			$sql = "SELECT * FROM equipment INNER JOIN participants ON equipment.ParticipantID = participants.ParticipantID WHERE EquipmentStatus = 'Approve'";
 			$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
 			while( $row = mysqli_fetch_assoc($resultset) ) 
 			{

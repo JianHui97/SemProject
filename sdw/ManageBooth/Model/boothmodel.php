14f5f20e4a4acef4087613bcb07ccb917a9031b7
jiasoonn <66350135+jiasoonn@users.noreply.github.com>
2020-08-01 16:08:25 +0800

    fix insertion bug

--- sdw/ManageBooth/Model/boothmodel.php
+++ sdw/ManageBooth/Model/boothmodel.php
@@ -1,14 +1,14 @@
 <?php
     require 'connection.php';
 	mysqli_select_db($conn, "ims");
-	
-	
+
+
 	class booth
 	{
 		public $BoothID;
 		public $Boothlocation;
 		public $ParticipantID;
-		
+
 		/* function viewbooth(){
 			global $conn;
 			$query = "SELECT *FROM booth
@@ -16,34 +16,34 @@
 			$result = mysqli_query($conn, $query) or die("Error:". mysqli_error($conn));
 			while($row=mysqli_fetch_assoc($result))
 			{
-				
+
 			}
 			return $result;
 		} */
-		
+
 		function updatebooth($bid, $newbloc){
 			global $conn;
-			$update = "UPDATE booth 
+			$update = "UPDATE booth
 					SET BoothLocation='".$newbloc.
 					"'WHERE BoothID='".$bid."'";
 			$result = mysqli_query($conn, $update);
 			return $result;
 		}
-		
+
 		function removebooth($selectedbid){
 			global $conn;
 			$delete = "DELETE FROM booth WHERE BoothID=".$selectedbid;
 			$result = mysqli_query($conn, $delete);
 			return $result;
 		}
-		
+
 		function newbooth($participantsid, $boothlocation){
 			global $conn;
-			$add = "INSERT INTO `booth` (`BoothLocation`,`ParticipantID`)VALUES('$boothlocation','$participantsid')";
+			$add = "INSET INTO booth (BoothLocation,ParticipantID)VALUES('$boothlocation','$participantsid')";
 			$result = mysqli_query($conn, $add);
 			return $result;
 		}
-		
+
 		/* function viewparticipants(){
 			global $conn;
 			$query = "SELECT * FROM participants";
